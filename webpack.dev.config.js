214dfe5e3c4178ef129e58cefb24eec19419fa41
Ivan Fediakov <ivan_fediakov@epam.com>
2019-07-12 14:08:36 +0300

    add loaders for sass, sass-modules, css, files

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,35 +1,110 @@
-const path = require('path');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: 'bundle.js',
-    },
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                use: ['babel-loader']
-            },
-            {
-                test: /\.html$/,
-                use: ['html-loader']
-            },
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    publicPath: '/',
+    filename: "bundle.js"
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: false
+            }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              ident: "postcss",
+              plugins: [require("autoprefixer")()]
+            }
+          }
         ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/index.html",
-            filename: "./index.html"
-        }),
-    ],
-    devtool: 'source-map',
-    stats: 'errors-only',
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        port: 9000
-    }
-}
+      },
+      {
+        test: /\.sass$/,
+        exclude: /\.module\.sass$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: false
+            }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              ident: "postcss",
+              plugins: [require("autoprefixer")()]
+            }
+          },
+          "sass-loader",
+        ]
+      },
+      {
+        test: /\.module.sass$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            query: {
+              localIdentName: "[hash:8]",
+              modules: true
+            }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              ident: "postcss",
+              plugins: [require("autoprefixer")()]
+            }
+          },
+          "sass-loader"
+        ],
+        test: /\.png|jpg|jpeg|svg|gif$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 10000
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ],
+  devtool: "source-map",
+  stats: "errors-only",
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    port: 9000,
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: [".webpack.js", ".js", ".jsx"]
+  }
+};
