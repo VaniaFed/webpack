66426a07b3be5ccbd76e8a720150fded8d702510
Ivan Fediakov <ivan_fediakov@epam.com>
2019-07-15 16:07:42 +0300

    add chunks optimization

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -35,7 +35,29 @@ module.exports = {
         })
     ],
     optimization: {
-        minimizer: [new OptimizeCSSAssetsPlugin()]
+        minimizer: [new OptimizeCSSAssetsPlugin()],
+        splitChunks: {
+            chunks: 'async',
+            minSize: 30000,
+            maxSize: 0,
+            minChunks: 1,
+            maxAsyncRequests: 5,
+            maxInitialRequests: 3,
+            automaticNameDelimiter: '~',
+            automaticNameMaxLength: 30,
+            name: true,
+            cacheGroups: {
+                vendors: {
+                    test: /[\\/]node_modules[\\/]/,
+                    priority: -10
+                },
+                default: {
+                    minChunks: 2,
+                    priority: -20,
+                    reuseExistingChunk: true
+                }
+            }
+        }
     },
     stats: 'errors-only',
     resolve: {
